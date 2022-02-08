be67753d69adc40f1b9097e5f34428af47318cb1
morganmcleod1123 <70032747+morganmcleod1123@users.noreply.github.com>
2022-02-08 07:41:07 -0600

    Removed Comment

--- src/checkers/searchers/OneLevelGreedy.java
+++ src/checkers/searchers/OneLevelGreedy.java
@@ -25,7 +25,6 @@ public class OneLevelGreedy extends CheckersSearcher {
         Optional<Duple<Integer, Move>> best = Optional.empty();
         for (Checkerboard alternative: board.getNextBoards()) {
             numNodes += 1;
-            // If the current player does not capture/get to go twice, negation = -1, else = 1.
             int negation = board.getCurrentPlayer() != alternative.getCurrentPlayer() ? -1 : 1;
             int scoreFor = negation * getEvaluator().applyAsInt(alternative);
             if (best.isEmpty() || best.get().getFirst() < scoreFor) {
